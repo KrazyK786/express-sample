ef454ddb1a5474169e994a88351760d6674ff040
Kristopher Johnson <krazyk786@gmail.com>
2020-02-24 13:59:20 -0600

    added handler for post route to get email

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ let landing = require('../controllers/landing');
 /* GET home page. */
 router.get('/', landing.get_landing);
 
+// POST for landing page
+router.post('/', landing.submit_lead);
+
 module.exports = router;
