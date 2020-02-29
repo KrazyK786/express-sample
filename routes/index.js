6bc19359b73ddc6d52afcfdbed4f8174dc880211
Kristopher Johnson <krazyk786@gmail.com>
2020-02-29 13:04:43 -0600

    implemented lead route to display lead details

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,10 @@ router.get('/', landing.get_landing);
 // POST for landing page
 router.post('/', landing.submit_lead);
 
-// GET leads
+// GET all leads
 router.get('/leads', landing.show_leads);
+
+// GET single lead
+router.get('/lead/:lead_id', landing.show_lead);
 
 module.exports = router;
