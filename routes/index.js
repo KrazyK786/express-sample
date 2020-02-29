f463e4991e419e0efccfe4889e040618aaf4832c
Kristopher Johnson <krazyk786@gmail.com>
2020-02-29 12:11:30 -0600

    implemented leads route to display all leads

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,7 @@ router.get('/', landing.get_landing);
 // POST for landing page
 router.post('/', landing.submit_lead);
 
+// GET leads
+router.get('/leads', landing.show_leads);
+
 module.exports = router;
